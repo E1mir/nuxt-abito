088e45263badaa2cc8b4ad4418a661d2752c3eb0
dmwebs <81981174+dmwebs@users.noreply.github.com>
2021-08-12 22:36:57 +0300

    feat: add Header component

---
+++ components/core/Header.vue
@@ -0,0 +1,16 @@
+<template>
+	<header class="header">
+		Header
+	</header>
+</template>
+
+<script>
+export default {
+	name: 'Header'
+}
+</script>
+
+<style lang="sass" scoped>
+	.header
+		font-size: 12px
+</style>
