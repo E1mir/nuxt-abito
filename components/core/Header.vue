c5efdd742fdbb1ac4e0686dc996e8cfb64179b2e
Elmir Ismayilzade <devkryternext@gmail.com>
2021-08-13 00:37:59 +0400

    chore: update Header component

--- components/core/Header.vue
+++ components/core/Header.vue
@@ -1,12 +1,67 @@
 <template>
 	<header class="header">
-		Header
+		<div class='header__left'>
+      <NuxtLink class='brand' to='/'>
+        <img src='/img/logo.svg' alt='Abito'>
+      </NuxtLink>
+      <nav>
+        <ul>
+          <li></li>
+        </ul>
+      </nav>
+    </div>
+    <div class='header__right'>
+      <div>
+        Вход и регистрация
+      </div>
+      <AppButton
+        v-if='show'
+        type='white'
+        :disabled='buttonDisabled'
+        @click='onPostAnAd'
+      >
+        Подать объявление
+      </AppButton>
+      <div v-else>
+        Скрыто
+      </div>
+      <br>
+      <br>
+      <AppButton @click='toggleButton'> Toggle button </AppButton>
+      <br>
+      <br>
+      <AppButton @click='toggleButtonShow'> Toggle hide</AppButton>
+    </div>
 	</header>
 </template>
 
 <script>
+import AppButton from '~/components/uikit/AppButton'
 export default {
-	name: 'Header'
+	name: 'Header',
+
+  components: {
+	  AppButton
+	},
+
+  data () {
+	  return {
+	    buttonDisabled: false,
+      show: true
+    }
+  },
+
+  methods: {
+	  onPostAnAd () {
+	    console.log('Open ad page')
+    },
+    toggleButton () {
+	    this.buttonDisabled = !this.buttonDisabled
+    },
+    toggleButtonShow () {
+	    this.show = !this.show
+    }
+  }
 }
 </script>
 
