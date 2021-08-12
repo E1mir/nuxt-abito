529846a3e88f49b6ad031d68bd629f053959f50a
Elmir Ismayilzade <devkryternext@gmail.com>
2021-08-13 00:39:35 +0400

    feat: add Product component

---
+++ components/product/Product.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class='product'>
+    {{title}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Product',
+
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
