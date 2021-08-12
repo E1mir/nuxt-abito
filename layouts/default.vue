ed49898b089e95fd573c44c64420c63fad97f627
dmwebs <81981174+dmwebs@users.noreply.github.com>
2021-08-12 22:37:20 +0300

    feat: add default layout

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+	<main>
+		<Header/>
+		<Nuxt/>
+	</main>
+</template>
+
+<script>
+import Header from '@/components/core/Header.vue';
+export default {
+	components: {
+		Header
+	}
+}
+</script>
+
+<style>
+
+</style>
