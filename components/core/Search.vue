3586896db9df4caf5e68d8284f89e0cff5085a8e
dmwebs <81981174+dmwebs@users.noreply.github.com>
2021-08-15 18:43:31 +0300

    feat Search and ProductItem

---
+++ components/core/Search.vue
@@ -0,0 +1,43 @@
+<template>
+	<div class="container">
+		<div class="search">
+			<input type="text" class="input search-input t14" name="search" placeholder="Поиск по объявлениям">
+			<AppButton class="search-btn"
+				:buttonImg="true" imgSrc="/img/search.svg"
+      >
+        Найти
+      </AppButton>
+		</div>
+	</div>
+</template>
+
+<script>
+import AppButton from '~/components/uikit/AppButton'
+export default {
+		name: 'Search',
+		components: {
+			AppButton
+		},
+}
+</script>
+
+<style lang="scss" scoped>
+	@import "assets/scss/variables";
+	.search{
+		display: flex;
+		justify-content: space-between;
+		background: $color-primary;
+		border-radius: 5px;
+		&-input{
+			width: 1036px;
+			margin: 3px;
+			
+		}
+		&-btn{
+			// width: 66px;
+			display: flex;
+			align-items: center;
+		}
+	}
+
+</style>
